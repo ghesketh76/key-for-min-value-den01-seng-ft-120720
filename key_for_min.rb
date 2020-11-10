b9ef6d88d8015775004c2d1c72c590fe39c6b7b2
Grant Hesketh <ghesketh76@gmail.com>
2020-11-10 19:08:25 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -6,15 +6,17 @@ def key_for_min_value(name_hash)
   min_value = nil
   name = nil
   name_hash.each do |key, value|
-    if min_value == nil 
-      min_value = value
-      name = key
-    else
-     if min_value > value
-      min_value = value
-      name = key
-    end
-    end
+  #  if min_value == nil 
+  #    min_value = value
+  #    name = key
+  #  else
+  #   if min_value > value
+  #    min_value = value
+  #    name = key
+  #  end
+  #  end
+  
+  
   end
   name
       
